61639c65d5708dc8f2d6249dd8e5f403ea01ce02
hoss70237-afk <hoss70237@gmail.com>
2026-07-13 07:39:37 +0900

    Add MainForm.Designer.cs with UI components

---
+++ UI/MainForm.Designer.cs
@@ -0,0 +1,137 @@
+namespace UsbInputMapper.UI
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ListBox lstProfiles;
+        private System.Windows.Forms.ListBox lstBindings;
+        private System.Windows.Forms.Button btnAddProfile;
+        private System.Windows.Forms.Button btnAddBinding;
+        private System.Windows.Forms.Button btnDeleteBinding;
+        private System.Windows.Forms.CheckBox chkStartup;
+        private System.Windows.Forms.Label lblProfiles;
+        private System.Windows.Forms.Label lblBindings;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lstProfiles = new System.Windows.Forms.ListBox();
+            this.lstBindings = new System.Windows.Forms.ListBox();
+            this.btnAddProfile = new System.Windows.Forms.Button();
+            this.btnAddBinding = new System.Windows.Forms.Button();
+            this.btnDeleteBinding = new System.Windows.Forms.Button();
+            this.chkStartup = new System.Windows.Forms.CheckBox();
+            this.lblProfiles = new System.Windows.Forms.Label();
+            this.lblBindings = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstProfiles
+            // 
+            this.lstProfiles.FormattingEnabled = true;
+            this.lstProfiles.ItemHeight = 12;
+            this.lstProfiles.Location = new System.Drawing.Point(12, 24);
+            this.lstProfiles.Name = "lstProfiles";
+            this.lstProfiles.Size = new System.Drawing.Size(150, 220);
+            this.lstProfiles.TabIndex = 0;
+            this.lstProfiles.SelectedIndexChanged += new System.EventHandler(this.lstProfiles_SelectedIndexChanged);
+            // 
+            // lstBindings
+            // 
+            this.lstBindings.FormattingEnabled = true;
+            this.lstBindings.ItemHeight = 12;
+            this.lstBindings.Location = new System.Drawing.Point(168, 24);
+            this.lstBindings.Name = "lstBindings";
+            this.lstBindings.Size = new System.Drawing.Size(350, 220);
+            this.lstBindings.TabIndex = 1;
+            // 
+            // btnAddProfile
+            // 
+            this.btnAddProfile.Location = new System.Drawing.Point(12, 250);
+            this.btnAddProfile.Name = "btnAddProfile";
+            this.btnAddProfile.Size = new System.Drawing.Size(150, 23);
+            this.btnAddProfile.TabIndex = 2;
+            this.btnAddProfile.Text = "プロファイル追加";
+            this.btnAddProfile.UseVisualStyleBackColor = true;
+            this.btnAddProfile.Click += new System.EventHandler(this.btnAddProfile_Click);
+            // 
+            // btnAddBinding
+            // 
+            this.btnAddBinding.Location = new System.Drawing.Point(168, 250);
+            this.btnAddBinding.Name = "btnAddBinding";
+            this.btnAddBinding.Size = new System.Drawing.Size(150, 23);
+            this.btnAddBinding.TabIndex = 3;
+            this.btnAddBinding.Text = "入力待機 (登録)";
+            this.btnAddBinding.UseVisualStyleBackColor = true;
+            this.btnAddBinding.Click += new System.EventHandler(this.btnAddBinding_Click);
+            // 
+            // btnDeleteBinding
+            // 
+            this.btnDeleteBinding.Location = new System.Drawing.Point(324, 250);
+            this.btnDeleteBinding.Name = "btnDeleteBinding";
+            this.btnDeleteBinding.Size = new System.Drawing.Size(100, 23);
+            this.btnDeleteBinding.TabIndex = 4;
+            this.btnDeleteBinding.Text = "登録削除";
+            this.btnDeleteBinding.UseVisualStyleBackColor = true;
+            this.btnDeleteBinding.Click += new System.EventHandler(this.btnDeleteBinding_Click);
+            // 
+            // chkStartup
+            // 
+            this.chkStartup.AutoSize = true;
+            this.chkStartup.Location = new System.Drawing.Point(430, 254);
+            this.chkStartup.Name = "chkStartup";
+            this.chkStartup.Size = new System.Drawing.Size(95, 16);
+            this.chkStartup.TabIndex = 5;
+            this.chkStartup.Text = "Windows起動時に実行";
+            this.chkStartup.UseVisualStyleBackColor = true;
+            this.chkStartup.CheckedChanged += new System.EventHandler(this.chkStartup_CheckedChanged);
+            // 
+            // lblProfiles
+            // 
+            this.lblProfiles.AutoSize = true;
+            this.lblProfiles.Location = new System.Drawing.Point(12, 9);
+            this.lblProfiles.Name = "lblProfiles";
+            this.lblProfiles.Size = new System.Drawing.Size(65, 12);
+            this.lblProfiles.TabIndex = 6;
+            this.lblProfiles.Text = "プロファイル:";
+            // 
+            // lblBindings
+            // 
+            this.lblBindings.AutoSize = true;
+            this.lblBindings.Location = new System.Drawing.Point(166, 9);
+            this.lblBindings.Name = "lblBindings";
+            this.lblBindings.Size = new System.Drawing.Size(53, 12);
+            this.lblBindings.TabIndex = 7;
+            this.lblBindings.Text = "入力一覧:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 286);
+            this.Controls.Add(this.lblBindings);
+            this.Controls.Add(this.lblProfiles);
+            this.Controls.Add(this.chkStartup);
+            this.Controls.Add(this.btnDeleteBinding);
+            this.Controls.Add(this.btnAddBinding);
+            this.Controls.Add(this.btnAddProfile);
+            this.Controls.Add(this.lstBindings);
+            this.Controls.Add(this.lstProfiles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UsbInputMapper - 設定";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}

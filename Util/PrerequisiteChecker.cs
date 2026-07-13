9ad427d5acc4645730ea999af0f4ccb95267a018
hoss70237-afk <hoss70237@gmail.com>
2026-07-13 16:42:17 +0900

    Update PrerequisiteChecker.cs

--- Util/PrerequisiteChecker.cs
+++ Util/PrerequisiteChecker.cs
@@ -13,14 +13,16 @@ namespace UsbInputMapper.Util
 
         private static bool CheckViGEmBus()
         {
-            // WindowsのシステムディレクトリにあるViGEmBusのドライバファイルを簡易チェック
             string system32 = Environment.GetFolderPath(Environment.SpecialFolder.System);
             string driverPath = Path.Combine(system32, @"drivers\ViGEmBus.sys");
 
             if (!File.Exists(driverPath))
             {
                 var result = MessageBox.Show(
-                    "ViGEmBusドライバが見つかりません。\r\n仮想ゲームパッドの出力機能が利用できない可能性があります。\r\n\r\n起動を続行しますか？",
+                    "ViGEmBusドライバが見つかりません。\r\n" +
+                    "仮想コントローラー(Xbox出力)機能を使用するには、ViGEmBusのインストールが必要です。\r\n\r\n" +
+                    "※キーボードやマウスの出力のみを使用する場合は、このまま「はい」を押して続行できます。\r\n\r\n" +
+                    "起動を続行しますか？",
                     "UsbInputMapper - 環境警告",
                     MessageBoxButtons.YesNo,
                     MessageBoxIcon.Warning);

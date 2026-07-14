cb32411e69da2c469c446945d635f15b83334367
hoss70237-afk <hoss70237@gmail.com>
2026-07-15 07:02:15 +0900

    Introduce StepPressState enum and PressState property
    Added StepPressState enum and PressState property to MacroStep class.

--- Profiles/MacroStep.cs
+++ Profiles/MacroStep.cs
@@ -3,22 +3,26 @@ using System.Collections.Generic;
 
 namespace UsbInputMapper.Profiles
 {
+    public enum StepPressState { Tap, Down, Up }
+
     public class MacroStep
     {
         public ActionType ActionType { get; set; }
         public int ArgumentNum { get; set; } 
-        public List<int> MultipleKeys { get; set; } // ★追加
+        public List<int> MultipleKeys { get; set; }
         public string ArgumentStr { get; set; } 
         public int MouseX { get; set; }
         public int MouseY { get; set; }
-        public bool IsAbsolutePosition { get; set; } 
         public int DelayMs { get; set; }
+        
+        public StepPressState PressState { get; set; } // ★追加: 押す/離す/タップ
 
         public MacroStep()
         {
             MultipleKeys = new List<int>();
             ActionType = ActionType.None;
             DelayMs = 50;
+            PressState = StepPressState.Tap;
         }
     }
 }

9e27dce42298fb9c634c710f1dc4b3af5be29b1f
hoss70237-afk <hoss70237@gmail.com>
2026-07-15 04:22:08 +0900

    Update MacroStep.cs

--- Profiles/MacroStep.cs
+++ Profiles/MacroStep.cs
@@ -1,23 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 namespace UsbInputMapper.Profiles
 {
     public class MacroStep
     {
         public ActionType ActionType { get; set; }
-        public int ArgumentNum { get; set; } // キーコードやボタンID
-        public string ArgumentStr { get; set; } // 起動パスなど
-        
-        // マウス移動用
+        public int ArgumentNum { get; set; } 
+        public List<int> MultipleKeys { get; set; } // ★追加
+        public string ArgumentStr { get; set; } 
         public int MouseX { get; set; }
         public int MouseY { get; set; }
-        public bool IsAbsolutePosition { get; set; } // true=デスクトップ絶対座標, false=相対移動
-
-        // 待機時間 (一括再生時のディレイ)
+        public bool IsAbsolutePosition { get; set; } 
         public int DelayMs { get; set; }
 
         public MacroStep()
         {
+            MultipleKeys = new List<int>();
             ActionType = ActionType.None;
             DelayMs = 50;
         }

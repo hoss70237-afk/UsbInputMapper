66755136c1a16f20c6e34aa6fc1d0938bbe0b064
hoss70237-afk <hoss70237@gmail.com>
2026-07-15 04:23:22 +0900

    Update CaptureForm.cs

--- UI/CaptureForm.cs
+++ UI/CaptureForm.cs
@@ -1,19 +1,32 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 using UsbInputMapper.Core;
 
 namespace UsbInputMapper.UI
 {
+    public enum CaptureMode { SingleAny, MultiKeyboard }
+
     public partial class CaptureForm : Form
     {
         public static bool IsCapturing { get; private set; }
         public static CaptureForm CurrentInstance { get; private set; }
         
+        public CaptureMode Mode { get; set; }
         public InputEvent CapturedEvent { get; private set; }
+        public List<int> CapturedKeys { get; private set; } = new List<int>();
 
-        public CaptureForm()
+        private int _downCount = 0;
+
+        public CaptureForm(CaptureMode mode = CaptureMode.SingleAny)
         {
             InitializeComponent();
+            Mode = mode;
+            if (Mode == CaptureMode.MultiKeyboard)
+            {
+                label1.Text = "キーボードのキーを押してください。\r\nすべてのキーを離すと確定します。";
+            }
         }
 
         private void CaptureForm_Load(object sender, EventArgs e)
@@ -28,7 +41,6 @@ namespace UsbInputMapper.UI
             if (CurrentInstance == this) CurrentInstance = null;
         }
 
-        // RawInputManagerから呼ばれるメソッド
         public void ProcessInput(InputEvent e)
         {
             if (this.InvokeRequired)
@@ -37,12 +49,36 @@ namespace UsbInputMapper.UI
                 return;
             }
 
-            // 押された瞬間のみをキャプチャし、離された時の信号は無視して確実に入力を捉える
-            if (e.IsKeyDown)
+            if (Mode == CaptureMode.SingleAny)
             {
-                CapturedEvent = e;
-                this.DialogResult = DialogResult.OK;
-                this.Close();
+                if (e.IsKeyDown)
+                {
+                    CapturedEvent = e;
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                }
+            }
+            else if (Mode == CaptureMode.MultiKeyboard)
+            {
+                if (e.Type == 1) // Keyboardのみ
+                {
+                    if (e.IsKeyDown)
+                    {
+                        if (!CapturedKeys.Contains(e.VKey)) CapturedKeys.Add(e.VKey);
+                        _downCount++;
+                        string keysStr = string.Join(" + ", CapturedKeys.Select(k => ((Keys)k).ToString()));
+                        label1.Text = $"取得中: {keysStr}";
+                    }
+                    else
+                    {
+                        _downCount--;
+                        if (_downCount <= 0 && CapturedKeys.Count > 0)
+                        {
+                            this.DialogResult = DialogResult.OK;
+                            this.Close();
+                        }
+                    }
+                }
             }
         }
 

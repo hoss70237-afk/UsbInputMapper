dfb25269a0896c5e4aa09ab9748cf224fcd4d54b
hoss70237-afk <hoss70237@gmail.com>
2026-07-20 01:08:47 +0900

    Update ViGEmOutput.cs

--- Core/ViGEmOutput.cs
+++ Core/ViGEmOutput.cs
@@ -26,7 +26,17 @@ namespace UsbInputMapper.Core
         public void Reset()
         {
             if (!IsInitialized) return;
-            foreach(Xbox360Button b in Enum.GetValues(typeof(Xbox360Button))) _controller.SetButtonState(b, false);
+            
+            // ★修正: Enum.GetValues ではなく明示的にボタンの配列を用意する
+            var buttons = new[] {
+                Xbox360Button.Up, Xbox360Button.Down, Xbox360Button.Left, Xbox360Button.Right,
+                Xbox360Button.Start, Xbox360Button.Back, Xbox360Button.LeftThumb, Xbox360Button.RightThumb,
+                Xbox360Button.LeftShoulder, Xbox360Button.RightShoulder, Xbox360Button.Guide,
+                Xbox360Button.A, Xbox360Button.B, Xbox360Button.X, Xbox360Button.Y
+            };
+            
+            foreach (var b in buttons) _controller.SetButtonState(b, false);
+
             _controller.SetAxisValue(Xbox360Axis.LeftThumbX, 0);
             _controller.SetAxisValue(Xbox360Axis.LeftThumbY, 0);
             _controller.SetAxisValue(Xbox360Axis.RightThumbX, 0);

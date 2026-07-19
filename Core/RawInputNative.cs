dd1a71cc989bc210ad9b0e1aaf2998b27216f2d2
hoss70237-afk <hoss70237@gmail.com>
2026-07-19 22:01:31 +0900

    Update RawInputNative.cs

--- Core/RawInputNative.cs
+++ Core/RawInputNative.cs
@@ -37,26 +37,17 @@ namespace UsbInputMapper.Core
             public IntPtr wParam;
         }
 
-        // 修正: 共用体メモリを正確にマッピングし、ボタンのフラグとホイールデータを正しく抽出できるようにした
         [StructLayout(LayoutKind.Explicit)]
         public struct RAWMOUSE
         {
-            [FieldOffset(0)]
-            public ushort usFlags;
-            [FieldOffset(4)]
-            public uint ulButtons;
-            [FieldOffset(4)]
-            public ushort usButtonFlags;
-            [FieldOffset(6)]
-            public short usButtonData;
-            [FieldOffset(8)]
-            public uint ulRawButtons;
-            [FieldOffset(12)]
-            public int lLastX;
-            [FieldOffset(16)]
-            public int lLastY;
-            [FieldOffset(20)]
-            public uint ulExtraInformation;
+            [FieldOffset(0)] public ushort usFlags;
+            [FieldOffset(4)] public uint ulButtons;
+            [FieldOffset(4)] public ushort usButtonFlags;
+            [FieldOffset(6)] public short usButtonData;
+            [FieldOffset(8)] public uint ulRawButtons;
+            [FieldOffset(12)] public int lLastX;
+            [FieldOffset(16)] public int lLastY;
+            [FieldOffset(20)] public uint ulExtraInformation;
         }
 
         [StructLayout(LayoutKind.Sequential)]

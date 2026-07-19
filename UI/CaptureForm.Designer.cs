b058a20aa80e0927208e929b9fadba4162b32696
hoss70237-afk <hoss70237@gmail.com>
2026-07-20 01:54:29 +0900

    Rename btnGestureEdge to btnRadialMenuEdge

--- UI/CaptureForm.Designer.cs
+++ UI/CaptureForm.Designer.cs
@@ -5,23 +5,23 @@ namespace UsbInputMapper.UI
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.Button btnGestureEdge; // ★追加
+        private System.Windows.Forms.Button btnRadialMenuEdge;
 
         protected override void Dispose(bool disposing) { if (disposing && (components != null)) components.Dispose(); base.Dispose(disposing); }
 
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label(); this.btnCancel = new System.Windows.Forms.Button(); this.btnGestureEdge = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label(); this.btnCancel = new System.Windows.Forms.Button(); this.btnRadialMenuEdge = new System.Windows.Forms.Button();
             this.SuspendLayout();
             
             this.label1.AutoSize = true; this.label1.Font = new System.Drawing.Font("MS UI Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128))); this.label1.Location = new System.Drawing.Point(20, 20); this.label1.Size = new System.Drawing.Size(206, 16); this.label1.Text = "デバイスの入力を待機しています...";
             
             this.btnCancel.Location = new System.Drawing.Point(20, 70); this.btnCancel.Size = new System.Drawing.Size(90, 23); this.btnCancel.Text = "キャンセル"; this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             
-            this.btnGestureEdge.Location = new System.Drawing.Point(120, 70); this.btnGestureEdge.Size = new System.Drawing.Size(140, 23); this.btnGestureEdge.Text = "ジェスチャー / ベゼル..."; this.btnGestureEdge.Click += new System.EventHandler(this.btnGestureEdge_Click);
+            this.btnRadialMenuEdge.Location = new System.Drawing.Point(120, 70); this.btnRadialMenuEdge.Size = new System.Drawing.Size(140, 23); this.btnRadialMenuEdge.Text = "ラジアルメニュー / ベゼル..."; this.btnRadialMenuEdge.Click += new System.EventHandler(this.btnRadialMenuEdge_Click);
             
             this.ClientSize = new System.Drawing.Size(284, 111);
-            this.Controls.Add(this.btnGestureEdge); this.Controls.Add(this.btnCancel); this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRadialMenuEdge); this.Controls.Add(this.btnCancel); this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow; this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent; this.Text = "入力待機"; this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.CaptureForm_FormClosed); this.Load += new System.EventHandler(this.CaptureForm_Load);
             this.ResumeLayout(false); this.PerformLayout();
         }

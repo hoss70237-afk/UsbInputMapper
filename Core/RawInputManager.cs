88d8a636d3118a00aba1fd31df5bed5af25e5bcd
hoss70237-afk <hoss70237@gmail.com>
2026-07-26 04:55:57 +0900

    Refactor RawInputManager to use ConcurrentDictionary

--- Core/RawInputManager.cs
+++ Core/RawInputManager.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
@@ -8,10 +8,10 @@ namespace UsbInputMapper.Core
     public class RawInputManager : NativeWindow, IDisposable
     {
         public event EventHandler<InputEvent> OnInputEvent;
-        public event EventHandler OnDeviceChanged; // ★ ホットプラグ対応
+        public event EventHandler OnDeviceChanged;
 
-        private readonly Dictionary<IntPtr, DeviceInfo> _devices = new Dictionary<IntPtr, DeviceInfo>();
-        private readonly Dictionary<IntPtr, byte[]> _lastHidData = new Dictionary<IntPtr, byte[]>();
+        private readonly ConcurrentDictionary<IntPtr, DeviceInfo> _devices = new ConcurrentDictionary<IntPtr, DeviceInfo>();
+        private readonly ConcurrentDictionary<IntPtr, byte[]> _lastHidData = new ConcurrentDictionary<IntPtr, byte[]>();
 
         public RawInputManager()
         {
@@ -26,7 +26,7 @@ namespace UsbInputMapper.Core
                 var rid = new RawInputNative.RAWINPUTDEVICE[1];
                 rid[0].usUsagePage = page;
                 rid[0].usUsage = usage;
-                rid[0].dwFlags = RawInputNative.RIDEV_INPUTSINK | RawInputNative.RIDEV_DEVNOTIFY; // WM_INPUT_DEVICE_CHANGE受信用
+                rid[0].dwFlags = RawInputNative.RIDEV_INPUTSINK | RawInputNative.RIDEV_DEVNOTIFY;
                 rid[0].hwndTarget = this.Handle;
 
                 RawInputNative.RegisterRawInputDevices(rid, 1, (uint)Marshal.SizeOf(typeof(RawInputNative.RAWINPUTDEVICE)));
@@ -49,7 +49,6 @@ namespace UsbInputMapper.Core
             if (m.Msg == RawInputNative.WM_INPUT) ProcessRawInput(m.LParam);
             else if (m.Msg == RawInputNative.WM_INPUT_DEVICE_CHANGE)
             {
-                // ★ デバイス構成の変更（ホットプラグ）を検知
                 OnDeviceChanged?.Invoke(this, EventArgs.Empty);
             }
             base.WndProc(ref m);
@@ -70,15 +69,21 @@ namespace UsbInputMapper.Core
                     var header = (RawInputNative.RAWINPUTHEADER)Marshal.PtrToStructure(pData, typeof(RawInputNative.RAWINPUTHEADER));
                     var devInfo = GetOrAddDeviceInfo(header.hDevice);
 
-                    InputEvent evt = new InputEvent { DeviceIdentifier = devInfo.GetIdentifier(), Type = (int)header.dwType };
-                    IntPtr pRawData = new IntPtr(pData.ToInt64() + (IntPtr.Size == 8 ? 24 : 16));
+                    InputEvent evt = new InputEvent 
+                    { 
+                        DeviceIdentifier = devInfo.GetIdentifier(), 
+                        Type = (int)header.dwType,
+                        Timestamp = Environment.TickCount64
+                    };
+                    
+                    IntPtr pRawData = new IntPtr(pData.ToInt64() + headerSize);
 
                     if (header.dwType == RawInputNative.RIM_TYPEKEYBOARD)
                     {
                         var kb = (RawInputNative.RAWKEYBOARD)Marshal.PtrToStructure(pRawData, typeof(RawInputNative.RAWKEYBOARD));
-                        evt.VKey = kb.VKey;
-                        evt.IsKeyDown = (kb.Message == 0x0100 || kb.Message == 0x0104);
-                        if (evt.VKey == 255) return;
+                        evt.Code = kb.VKey;
+                        evt.IsDown = (kb.Message == 0x0100 || kb.Message == 0x0104);
+                        if (evt.Code == 255) return;
                         OnInputEvent?.Invoke(this, evt);
                     }
                     else if (header.dwType == RawInputNative.RIM_TYPEMOUSE)
@@ -93,8 +98,8 @@ namespace UsbInputMapper.Core
 
                         if ((ms.usButtonFlags & 0x0400) != 0) 
                         {
-                            evt.MouseButtonFlags = (uint)(ms.usButtonData > 0 ? 4 : 5);
-                            evt.IsKeyDown = true;
+                            evt.Code = ms.usButtonData > 0 ? 4 : 5;
+                            evt.IsDown = true;
                             OnInputEvent?.Invoke(this, evt);
                         }
                     }
@@ -127,8 +132,12 @@ namespace UsbInputMapper.Core
                                             bool isDown = (rawData[i] & (1 << b)) != 0;
                                             InputEvent hidEvt = new InputEvent 
                                             {
-                                                DeviceIdentifier = evt.DeviceIdentifier, Type = 2,
-                                                MouseButtonFlags = (uint)customCode, IsKeyDown = isDown, HidData = rawData
+                                                DeviceIdentifier = evt.DeviceIdentifier, 
+                                                Type = 2,
+                                                Code = customCode, 
+                                                IsDown = isDown, 
+                                                HidData = rawData,
+                                                Timestamp = evt.Timestamp
                                             };
                                             OnInputEvent?.Invoke(this, hidEvt);
                                         }
@@ -144,28 +153,23 @@ namespace UsbInputMapper.Core
             finally { Marshal.FreeHGlobal(pData); }
         }
 
-        private void EmitMouseEvent(InputEvent baseEvt, uint currentFlags, uint downFlag, uint upFlag, uint mappedCode)
+        private void EmitMouseEvent(InputEvent baseEvt, uint currentFlags, uint downFlag, uint upFlag, int mappedCode)
         {
             if ((currentFlags & downFlag) != 0)
             {
-                InputEvent evt = new InputEvent { DeviceIdentifier = baseEvt.DeviceIdentifier, Type = baseEvt.Type, MouseButtonFlags = mappedCode, IsKeyDown = true };
+                InputEvent evt = new InputEvent { DeviceIdentifier = baseEvt.DeviceIdentifier, Type = baseEvt.Type, Code = mappedCode, IsDown = true, Timestamp = baseEvt.Timestamp };
                 OnInputEvent?.Invoke(this, evt);
             }
             else if ((currentFlags & upFlag) != 0)
             {
-                InputEvent evt = new InputEvent { DeviceIdentifier = baseEvt.DeviceIdentifier, Type = baseEvt.Type, MouseButtonFlags = mappedCode, IsKeyDown = false };
+                InputEvent evt = new InputEvent { DeviceIdentifier = baseEvt.DeviceIdentifier, Type = baseEvt.Type, Code = mappedCode, IsDown = false, Timestamp = baseEvt.Timestamp };
                 OnInputEvent?.Invoke(this, evt);
             }
         }
 
         private DeviceInfo GetOrAddDeviceInfo(IntPtr hDevice)
         {
-            if (!_devices.TryGetValue(hDevice, out var info))
-            {
-                info = new DeviceInfo { Handle = hDevice };
-                _devices[hDevice] = info;
-            }
-            return info;
+            return _devices.GetOrAdd(hDevice, h => new DeviceInfo { Handle = hDevice });
         }
 
         public void Dispose() { DestroyHandle(); }

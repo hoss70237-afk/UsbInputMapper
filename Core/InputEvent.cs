a3ab279ffbd6c0851ac364ebce2028f06ee6d81c
hoss70237-afk <hoss70237@gmail.com>
2026-07-26 04:54:37 +0900

    Update InputEvent.cs

--- Core/InputEvent.cs
+++ Core/InputEvent.cs
@@ -5,27 +5,52 @@ namespace UsbInputMapper.Core
     public class InputEvent
     {
         public string DeviceIdentifier { get; set; }
-        public int Type { get; set; } // 0:Mouse, 1:Keyboard, 2:HID
+        public int Type { get; set; } // 0:Mouse, 1:Keyboard, 2:HID, 10:PadBtn, 11:PadAxis, 12:POV, 5:Bezel
         
-        // Keyboard
-        public ushort VKey { get; set; }
-        public bool IsKeyDown { get; set; }
+        // VKeyやMouseButtonFlagsなどを一元管理するためのプロパティ
+        public int Code { get; set; }
+        
+        // AxisやPOVの値用
+        public int Value { get; set; }
+        
+        public bool IsDown { get; set; }
 
-        // Mouse
-        public uint MouseButtonFlags { get; set; }
-        public int MouseData { get; set; } // Wheel etc
-
-        // HID
+        public int X { get; set; } 
+        public int Y { get; set; }
         public byte[] HidData { get; set; }
+
+        // OSの起動ミリ秒（24.9日オーバーフロー対策済み）
+        public long Timestamp { get; set; }
+
+        // 後方互換性と利便性のためのラッパープロパティ
+        public ushort VKey 
+        { 
+            get => (ushort)Code; 
+            set => Code = value; 
+        }
+        
+        public uint MouseButtonFlags 
+        { 
+            get => (uint)Code; 
+            set => Code = (int)value; 
+        }
+        
+        public bool IsKeyDown 
+        { 
+            get => IsDown; 
+            set => IsDown = value; 
+        }
 
         public override string ToString()
         {
             if (Type == 1)
-                return $"KB: {VKey} ({(IsKeyDown ? "Down" : "Up")})";
+                return $"KB: {Code} ({(IsDown ? "Down" : "Up")})";
             else if (Type == 0)
-                return $"MS: Flags={MouseButtonFlags}";
-            else
+                return $"MS: Code={Code} ({(IsDown ? "Down" : "Up")})";
+            else if (Type == 2)
                 return $"HID: DataLen={(HidData != null ? HidData.Length : 0)}";
+            else
+                return $"Pad: Type={Type} Code={Code} Val={Value}";
         }
     }
 }

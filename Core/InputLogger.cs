6504bdbc542210d75148a1bf94f99b00f6caefff
hoss70237-afk <hoss70237@gmail.com>
2026-07-26 12:08:20 +0900

    Update InputLogger.cs

--- Core/InputLogger.cs
+++ Core/InputLogger.cs
@@ -1,15 +1,33 @@
+// FILE: Core/InputLogger.cs
 using System;
 
 namespace UsbInputMapper.Core
 {
+    public class DiagnosticEvent
+    {
+        public bool IsPhysical { get; set; }
+        public long Timestamp { get; set; }
+        public int Type { get; set; }
+        public int Code { get; set; }
+        public int Value { get; set; }
+        public bool IsDown { get; set; }
+    }
+
     public static class InputLogger
     {
         public static event Action<string> OnLog;
+        public static event Action<DiagnosticEvent> OnDiagnostic;
+        
         public static bool IsLoggingEnabled { get; set; } = false;
 
         public static void Log(string message)
         {
             if (IsLoggingEnabled) OnLog?.Invoke($"[{DateTime.Now:HH:mm:ss.fff}] {message}");
+        }
+
+        public static void LogDiagnostic(DiagnosticEvent evt)
+        {
+            if (IsLoggingEnabled) OnDiagnostic?.Invoke(evt);
         }
     }
 }

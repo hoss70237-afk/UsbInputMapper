c9b2e8ffbbedea493b4e66b33ba46fc5772c43fc
hoss70237-afk <hoss70237@gmail.com>
2026-07-27 02:56:24 +0900

    Update Profile.cs

--- Profiles/Profile.cs
+++ Profiles/Profile.cs
@@ -11,18 +11,20 @@ namespace UsbInputMapper.Profiles
         
         public bool EnableXInput { get; set; } = false;
         
-        // 通知機能の拡充
+        // 切替時通知
         public bool NotifyProfileChangeVibration { get; set; } = false;
         public bool NotifyProfileChangeBeep { get; set; } = false;
         public bool NotifyProfileChangeTTS { get; set; } = false;
         
+        // 個別チャタリング防止
         public bool OverrideGlobalChattering { get; set; } = false;
         public bool EnableChatteringCanceler { get; set; } = false;
         public int ChatteringThresholdMs { get; set; } = 20;
         
+        // OSD通知
         public bool OverlayShowMark { get; set; } = true;
         public bool OverlayShowName { get; set; } = true;
-        public int OverlayPosX { get; set; } = -1; // -1はデフォルト(右上)
+        public int OverlayPosX { get; set; } = -1; // -1: デフォルト(右上)
         public int OverlayPosY { get; set; } = -1;
         public int OverlayDurationMs { get; set; } = 2000;
 
@@ -30,6 +32,7 @@ namespace UsbInputMapper.Profiles
 
         public Profile()
         {
+            Name = "新規プロファイル";
             TargetApplicationPaths = new List<string>();
             Bindings = new List<Binding>();
         }

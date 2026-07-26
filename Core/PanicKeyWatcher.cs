0f583e8e56d81c669047052e03714ee0e27e75d1
hoss70237-afk <hoss70237@gmail.com>
2026-07-27 02:25:25 +0900

    Update PanicKeyWatcher.cs

--- Core/PanicKeyWatcher.cs
+++ Core/PanicKeyWatcher.cs
@@ -3,50 +3,76 @@ using System.Timers;
 
 namespace UsbInputMapper.Core
 {
-    public class PanicKeyWatcher
+    public class PanicKeyWatcher : IDisposable
     {
         public event EventHandler OnPanicTriggered;
 
         private bool _leftPressed;
         private bool _rightPressed;
         private Timer _timer;
+        private readonly object _lockObj = new object();
 
         public PanicKeyWatcher()
         {
-            _timer = new Timer(10000); // 10秒
+            _timer = new Timer(10000); // 10秒間連続保持で判定
             _timer.Elapsed += Timer_Elapsed;
             _timer.AutoReset = false;
         }
 
         public void ProcessMouseInput(uint flags)
         {
-            // RawInputのフラグ定数
-            // RI_MOUSE_LEFT_BUTTON_DOWN = 0x0001
-            // RI_MOUSE_LEFT_BUTTON_UP   = 0x0002
-            // RI_MOUSE_RIGHT_BUTTON_DOWN= 0x0004
-            // RI_MOUSE_RIGHT_BUTTON_UP  = 0x0008
-
-            if ((flags & 0x0001) != 0) _leftPressed = true;
-            if ((flags & 0x0002) != 0) _leftPressed = false;
-            if ((flags & 0x0004) != 0) _rightPressed = true;
-            if ((flags & 0x0008) != 0) _rightPressed = false;
-
-            if (_leftPressed && _rightPressed)
+            lock (_lockObj)
             {
-                if (!_timer.Enabled)
+                // RI_MOUSE_LEFT_BUTTON_DOWN = 0x0001
+                // RI_MOUSE_LEFT_BUTTON_UP   = 0x0002
+                // RI_MOUSE_RIGHT_BUTTON_DOWN= 0x0004
+                // RI_MOUSE_RIGHT_BUTTON_UP  = 0x0008
+
+                if ((flags & 0x0001) != 0) _leftPressed = true;
+                if ((flags & 0x0002) != 0) _leftPressed = false;
+                if ((flags & 0x0004) != 0) _rightPressed = true;
+                if ((flags & 0x0008) != 0) _rightPressed = false;
+
+                if (_leftPressed && _rightPressed)
                 {
-                    _timer.Start();
+                    if (!_timer.Enabled)
+                    {
+                        _timer.Start();
+                    }
                 }
-            }
-            else
-            {
-                _timer.Stop();
+                else
+                {
+                    if (_timer.Enabled)
+                    {
+                        _timer.Stop();
+                    }
+                }
             }
         }
 
         private void Timer_Elapsed(object sender, ElapsedEventArgs e)
         {
+            lock (_lockObj)
+            {
+                _timer.Stop();
+                _leftPressed = false;
+                _rightPressed = false;
+            }
             OnPanicTriggered?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Dispose()
+        {
+            lock (_lockObj)
+            {
+                if (_timer != null)
+                {
+                    _timer.Stop();
+                    _timer.Elapsed -= Timer_Elapsed;
+                    _timer.Dispose();
+                    _timer = null;
+                }
+            }
         }
     }
 }
